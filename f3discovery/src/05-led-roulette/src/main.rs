331042ba046b16d8f19e75c6ebc3ec1552ef3b9f
Mareq Balint <mareq@balint.eu>
2021-12-22 22:31:28 +0000

    Blink LEDs
    Rust Discovery book, chapter 5
    (up to and including [ch5.4](https://docs.rust-embedded.org/discovery/f3discovery/05-led-roulette/the-led-and-delay-abstractions.html))

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,14 +2,24 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
+use volatile;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut half_period = 128_u16;
+    let v_half_period = volatile::Volatile::new(&mut half_period);
+
+    loop {
+        for i in 0..8 {
+            leds[i].on().ok();
+            delay.delay_ms(v_half_period.read());
+        }
+        for i in 0..8 {
+            leds[i].off().ok();
+            delay.delay_ms(v_half_period.read());
+        }
+    }
 }
