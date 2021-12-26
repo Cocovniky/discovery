ed32c2abf109bfce0e9ab5554fb71bc643fec077
Mareq Balint <mareq@balint.eu>
2021-12-26 18:49:08 +0000

    Attempt to read from bad address
    Rust Discovery book, chapter 7
    (up to and including [ch7.3](https://docs.rust-embedded.org/discovery/f3discovery/07-registers/bad-address.html))

--- f3discovery/src/07-registers/src/main.rs
+++ f3discovery/src/07-registers/src/main.rs
@@ -8,12 +8,12 @@ use aux7::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let (mut itm, _) = aux7::init();
+
+    // A magic address!
+    const GPIOE_BSRR: u32 = 0x4800_1018;
 
     unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x4800_1018;
-
         // Turn on the "North" LED (red)
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
 
@@ -27,5 +27,17 @@ fn main() -> ! {
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
     }
 
+    unsafe {
+        // Read from (write-only) GPIO BSRR register address
+        let value = ptr::read_volatile(GPIOE_BSRR as *const u32);
+        iprintln!(&mut itm.stim[0], "{:#x}: {:#x}", GPIOE_BSRR, value);
+
+        // A bad address!
+        const BAD: u32 = 0x48001800;
+
+        // Read from bad address (no registers there)
+        let value = ptr::read_volatile(BAD as *const u32);
+        iprintln!(&mut itm.stim[0], "{:#x}: {:#x}", BAD, value);
+    }
     loop {}
 }
