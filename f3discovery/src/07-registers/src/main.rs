f70ad6ea8ff06aa158c402bf1ed0c54ab78a1d01
Mareq Balint <mareq@balint.eu>
2021-12-26 19:04:00 +0000

    Read state of GPIO pins from GPIO ODR register
    Rust Discovery book, chapter 7
    (up to and including [ch7.4](https://docs.rust-embedded.org/discovery/f3discovery/07-registers/spooky-action-at-a-distance.html))

--- f3discovery/src/07-registers/src/main.rs
+++ f3discovery/src/07-registers/src/main.rs
@@ -4,7 +4,7 @@
 use core::ptr;
 
 #[allow(unused_imports)]
-use aux7::{entry, iprint, iprintln};
+use aux7::{entry, iprint, iprintln, ITM};
 
 #[entry]
 fn main() -> ! {
@@ -14,17 +14,24 @@ fn main() -> ! {
     const GPIOE_BSRR: u32 = 0x4800_1018;
 
     unsafe {
+        // Print the initial contents of ODR
+        iprint_odr(&mut itm);
+
         // Turn on the "North" LED (red)
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+        iprint_odr(&mut itm);
 
         // Turn on the "East" LED (green)
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
+        iprint_odr(&mut itm);
 
         // Turn off the "North" LED
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
+        iprint_odr(&mut itm);
 
         // Turn off the "East" LED
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
+        iprint_odr(&mut itm);
     }
 
     unsafe {
@@ -40,4 +47,16 @@ fn main() -> ! {
         iprintln!(&mut itm.stim[0], "{:#x}: {:#x}", BAD, value);
     }
     loop {}
+}
+
+fn iprint_odr(itm: &mut ITM) {
+    const GPIOE_ODR: u32 = 0x4800_1014;
+
+    unsafe {
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read_volatile(GPIOE_ODR as *const u16)
+        );
+    }
 }
